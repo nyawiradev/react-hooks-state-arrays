c5294ab0c6b28c64b88d30227191c26f683531f7
nyawiradev <njaginjoyce@gmail.com>
2022-07-20 05:33:05 +0300

    initial commit

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,19 +1,45 @@
 import React, { useState } from "react";
-import { spicyFoods, getNewRandomSpicyFood } from "../data";
+import { spicyFoods, getNewSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
 
+
   function handleAddFood() {
-    const newFood = getNewRandomSpicyFood();
+    const newFood = getNewSpicyFood();
+    const newFoodArray = [...foods, newFood]
+    setFoods(newFoodArray)
     console.log(newFood);
   }
 
+  function handleLiClick(id) {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        } 
+      }else {
+          return food
+        }
+      
+    })
+setFoods(newFoodArray)
+  }
+
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id}
+    onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+
     </li>
-  ));
+  ))
+
+  const [filterBy, setFilterBy] = useState("All")
+
+  function handleFilterChange(event){
+    setFilterBy(event.target.value)
+  }
 
   return (
     <div>
